4a188da2e28b324a068a667fd78d5d6279786f29
JavierVz99 <javo39a123@gmail.com>
2022-06-20 01:14:48 -0500

    MireposJav

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(0);
+define('RUTA', 'https://tesiji.herokuapp.com/');
+
+// nombre de base de datos
+$bd_config = array(
+    'db_name' => 'bdzsucwzg5z6wtmfchs5',
+    'user' => 'ub0mlw08wef3doki',
+    'pass' => 'UsHxd7DJC1hgvsTVp62W'    
+);
+
+
+ ?>
